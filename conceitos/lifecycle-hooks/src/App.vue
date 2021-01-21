26174fc142242e04c25617bd836063b635e35be0
eRodriguesSantana <eduardo.r.santana@hotmail.com>
2021-01-21 12:09:11 -0300

    Estudo teórico e prático sobre Life-cycle Hooks

---
+++ conceitos/lifecycle-hooks/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Lifecycle Hooks</h1>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+
+/*
+export default {
+  mounted() {
+    console.log('Foi montado options API')
+  }
+}
+*/
+
+export default {
+  setup() {
+    onMounted(() => {
+      console.log('Foi montado composition API')
+    })
+  }  
+}
+</script>

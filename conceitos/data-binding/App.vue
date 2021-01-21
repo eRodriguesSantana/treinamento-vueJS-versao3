c35ab217014fe64c4ab37e28c72a0f223316d240
eRodriguesSantana <eduardo.r.santana@hotmail.com>
2021-01-21 11:52:34 -0300

    Estudo teórico e prático sobre Data-binding

---
+++ conceitos/data-binding/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 v-bind:style="{ textDecoration: decoration }">Hello {{ name }}</h1>
+    <input type="text" v-model="name">
+    <br>
+    <a v-bind:href="link">Link Para o Curso</a>
+  </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: 'Eduardo',
+        link: 'https://treinamento.vuejsbrasil.org',
+        decoration: 'underline'
+    })
+}
+</script>
